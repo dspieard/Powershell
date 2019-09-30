a72d162d11b405332ec99e0eeb8826b828a851d1
dspieard <37297387+dspieard@users.noreply.github.com>
2019-09-30 19:44:37 +0200

    try

--- Scripts/FirstScript.ps1
+++ Scripts/FirstScript.ps1
@@ -1,1 +1,7 @@
-"Hello World"
+if ($True) {
+    Write-Host -ForegroundColor Green "Hello!"
+}
+
+
+$AutomaticFile = Get-Item D:\Powershell\temp\Automatic
+if (!$AutomaticFile) {"$AutomaticFile does not exist"} else {"$AutomaticFile exists"}
