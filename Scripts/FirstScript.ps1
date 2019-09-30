9b4ffa2501a912050a0a25a164685bde4247e179
dspieard <37297387+dspieard@users.noreply.github.com>
2019-09-30 20:29:08 +0200

    test2

--- Scripts/FirstScript.ps1
+++ Scripts/FirstScript.ps1
@@ -1,7 +1,45 @@
+#If
 if ($True) {
     Write-Host -ForegroundColor Green "Hello!"
 }
 
-
+#Else
 $AutomaticFile = Get-Item D:\Powershell\temp\Automatic
 if (!$AutomaticFile) {"$AutomaticFile does not exist"} else {"$AutomaticFile exists"}
+
+if ($false){
+    Write-Host -ForegroundColor Green "This is IF"
+} else {
+    Write-Host -ForegroundColor Green "This is Else"
+}
+
+#Elseif
+if (!$true) {"If"} elseif ($true) { "Else if"} else {"What Else?"}
+
+#Switch
+$SpoolerService = Get-Service -Name "Spooler"
+Switch ((Get-Service Spooler).Status){
+    "Starting" { "Wait for the startup" }
+    "Running" { "It's already running" }
+    Default {$SpoolerService | Start-Service; Write-Host "Starting the service"}
+}
+
+#Functions
+
+Function Dosomething {
+<#
+.SYNOPSIS
+    What the function does
+.DESCRIPTION
+    All the information you need
+.EXAMPLE
+    An example
+    Description of what the example does
+#>
+
+    "Something"
+}
+#Call with 
+Dosomething
+
+
